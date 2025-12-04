cb0283195ab860a9af856a215bf040d1fecdb45f
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-12-05 04:51:58 +0900

    package.swift

--- Package.swift
+++ Package.swift
@@ -5,22 +5,39 @@ import PackageDescription
 
 let package = Package(
     name: "MUGE",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MUGE",
-            targets: ["MUGE"]
+            name: "MUGE.Artwork",
+            targets: ["Artwork"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Ars-Tools/fcikernel", from: .init(0, 0, 0))
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "Artwork-Preview",
+            dependencies: ["Artwork", "CIArtwork", "Procedure"],
+            path: "Artwork/Preview"
+        ),
         .target(
-            name: "MUGE"
+            name: "Artwork",
+            path: "Artwork/Sources"
         ),
-        .testTarget(
-            name: "MUGETests",
-            dependencies: ["MUGE"]
+        .target(
+            name: "CIArtwork",
+            dependencies: ["Artwork"],
+            path: "CIArtwork/Sources"
         ),
+        .target(
+            name: "Procedure",
+            dependencies: ["Artwork"],
+            path: "Procedure/Sources"
+        )
     ]
 )
