01d2feb0f0ad5f390364177fb0dccb322d30e735
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-12-05 04:52:57 +0900

    WIP

---
+++ CIArtwork/Sources/Generators.swift
@@ -0,0 +1,17 @@
+//
+//  Generators.swift
+//  MUGE
+//
+//  Created by Kota on 12/3/25.
+//
+@preconcurrency import CoreImage.CIFilterBuiltins
+@preconcurrency import protocol Combine.Publisher
+public func noise() -> some CIArtwork {
+    CIFilter.randomGenerator() as CIFilter
+}
+public func qr(message: Data) -> some CIArtwork {
+    let filter = CIFilter.qrCodeGenerator()
+    filter.message = message
+    filter.correctionLevel = "Q"
+    return filter as CIFilter
+}
