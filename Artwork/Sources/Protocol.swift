f2cd65ed9a85a589057c965fd56138ee6449e7f8
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-12-11 01:06:38 +0900

    update protocol

--- Artwork/Sources/Protocol.swift
+++ Artwork/Sources/Protocol.swift
@@ -4,9 +4,10 @@
 //
 //  Created by Kota on 12/3/25.
 //
-import protocol SwiftUI.Gesture
-@preconcurrency import Metal
-@preconcurrency import protocol Combine.Publisher
+@_exported import protocol SwiftUI.Gesture
+@_exported @preconcurrency import protocol Combine.Publisher
+@_exported @preconcurrency import Metal
+import func simd.min
 public protocol Artwork: Sendable {
     @inlinable
     func callAsFunction(as format: MTLPixelFormat, in residency: MTLResidencySet, signal: some Publisher<(SIMD2<Float64>, Gesture), Never>) throws -> @Sendable (CFTimeInterval, MTL4CommandBuffer, MTLTexture) -> Void
@@ -21,6 +22,28 @@ extension MTLClearColor: Artwork {
             descriptor.colorAttachments[0].texture = $2
             let encoder = $1.makeRenderCommandEncoder(descriptor: descriptor)
             encoder?.endEncoding()
+        }
+    }
+}
+public protocol MTLArtwork: Artwork {
+    @inlinable
+    func callAsFunction(in residency: MTLResidencySet, signal: some Publisher<(SIMD2<Float64>, Gesture), Never>) throws -> (MTLTextureDescriptor, @Sendable (CFTimeInterval, MTL4CommandBuffer) -> MTLTexture)
+}
+extension MTLArtwork {
+    public func callAsFunction(as format: MTLPixelFormat, in residency: any MTLResidencySet, signal: some Publisher<(SIMD2<Float64>, Gesture), Never>) throws -> @Sendable (CFTimeInterval, any MTL4CommandBuffer, any MTLTexture) -> Void {
+        let (descriptor, generator) = try callAsFunction(in: residency, signal: signal)
+        let w = Float64(descriptor.width)
+        let h = Float64(descriptor.height)
+        let blit = try Blit.Uni(with: residency.device.makeCompiler(descriptor: .init()),
+                                to: format, scale: min(.one, .init(.init(h / w), .init(w / h))), name: "RGB2RGB")
+        let resource = try residency.device.makeResidencySet(descriptor: .init())
+        return {
+            resource.removeAllAllocations()
+            let texture = generator($0, $1)
+            resource.addAllocation(texture)
+            blit.render(to: $2, with: $1, primary: texture)
+            resource.commit()
+            $1.useResidencySet(resource)
         }
     }
 }
